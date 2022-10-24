332a722473237862a55e253ef0f6f498fd35c2b4
Karpov Dmitry <dmikarpov@ozon.ru>
2022-10-24 16:31:59 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from typing import Optional, List
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel
+
+app = FastAPI(
+    title='fast api lms',
+    description='fast api lms app',
+    version='0.1',
+    contact={
+        'name': 'Karpov Dmitry',
+        'email': 'dkarpov@company.com',
+    }
+)
+
+
+class User(BaseModel):
+    email: str
+    is_active: bool
+    bio: Optional[str]
+
+
+users = [
+    User(email='u1@', is_active=True, bio='u1 bio'),
+    User(email='u2@', is_active=False, bio='u2 bio'),
+    User(email='u3@', is_active=False, bio='u3 bio here'),
+]
+
+
+@app.get("/users", response_model=List[User])
+async def get_users():
+    return users
+
+
+@app.post("/users")
+async def create_user(user: User):
+    users.append(user)
+    return user.email
+
+
+@app.get("/users/{user_id}")
+async def get_user(
+        user_id: int = Path(..., description='user id is expected here'),
+        q: str = Query(None, max_length=5)):
+    return {"user": users[user_id], "q": q}
