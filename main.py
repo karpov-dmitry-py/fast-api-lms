3acd388c5708c56b6f902508378d37f4c383e539
Karpov Dmitry <dmikarpov@ozon.ru>
2022-11-03 10:55:25 +0500

    api splitted into separate modules

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from typing import Optional, List
-from fastapi import FastAPI, Path, Query
-from pydantic import BaseModel
+from fastapi import FastAPI
+
+from api import users, courses, sections
 
 app = FastAPI(
     title='fast api lms',
@@ -12,33 +12,6 @@ app = FastAPI(
     }
 )
 
-
-class User(BaseModel):
-    email: str
-    is_active: bool
-    bio: Optional[str]
-
-
-users = [
-    User(email='u1@', is_active=True, bio='u1 bio'),
-    User(email='u2@', is_active=False, bio='u2 bio'),
-    User(email='u3@', is_active=False, bio='u3 bio here'),
-]
-
-
-@app.get("/users", response_model=List[User])
-async def get_users():
-    return users
-
-
-@app.post("/users")
-async def create_user(user: User):
-    users.append(user)
-    return user.email
-
-
-@app.get("/users/{user_id}")
-async def get_user(
-        user_id: int = Path(..., description='user id is expected here'),
-        q: str = Query(None, max_length=5)):
-    return {"user": users[user_id], "q": q}
+app.include_router(users.router)
+app.include_router(courses.router)
+app.include_router(sections.router)
